d050ed125fc10417b9cf0a2eb2bb261843be48de
Amanda Chaudhary <amanda@catsynth-appwerx.com>
2023-09-09 18:45:13 -0700

    Added open and insert commands

--- main.ts
+++ main.ts
@@ -1,33 +1,53 @@
+import { isMoment } from 'moment';
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { getAllDailyNotes, getDailyNote, createDailyNote } from  'obsidian-daily-notes-interface'
+import type { Moment } from "moment";
 
 // Remember to rename these classes and interfaces!
 
 export default class YesterdayPlugin extends Plugin {
-	settings: YesterdayPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
 
-		
-
+	
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-yesterday-note',
 			name: 'Open yesterday\'s Daily Note',
-			callback: () => {
-				
+			callback: async () => {
+				const today = moment()
+				const yesterday = today.subtract(1,'days')
+				const dailyNotes = getAllDailyNotes()
+				var note = getDailyNote(yesterday,dailyNotes)
+				console.log(note)
+				if (note != null) {
+					let leaf = this.app.workspace.getLeaf(false);
+					await leaf.openFile(note, { active: true });
+				} else {
+					note = await createDailyNote(yesterday)
+					let leaf = this.app.workspace.getLeaf(false);
+					await leaf.openFile(note, { active: true });
+				}
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'yesterday-editor-command',
 			name: 'Insert link to yesterday\'s Daily Note',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				const today = moment()
+				const yesterday = today.subtract(1,'days')
+				const dailyNotes = getAllDailyNotes()
+				var note = getDailyNote(yesterday,dailyNotes)
+				console.log(note)
+				if (note == null) {
+					note = await createDailyNote(yesterday)
+				}
+				editor.replaceSelection('[['+note.basename+']]')
 			}
 		});
-
+	}
 
 	
 
@@ -35,12 +55,5 @@ export default class YesterdayPlugin extends Plugin {
 
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
